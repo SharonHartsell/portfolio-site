3f98960756d9ea4f004330367ff2612486db953b
Sharon Hartsell <sharon.d.hartsell@gmail.com>
2019-10-07 21:54:26 -0400

    style(intro): establish basic layout for nav and intro section

--- index.php
+++ index.php
@@ -1,7 +1,36 @@
 <?php include("includes/html_header.inc"); ?>
 <?php include("includes/header.inc"); ?>
 
-<p><i class="fas fa-paper-plane"></i> Hello world! This is HTML5 Boilerplate.</p>
+<section class="intro">
+  <img src="https://via.placeholder.com/300" class="intro_image" alt="Sharon" />
+  <h1 class="intro_headline">Hi! I’m Sharon, a front-end web developer.</h1>
+  <ul class="intro_techList">
+    <li class="intro_techListItem">
+      <i class="fab fa-html5 intro_techIcon"></i>
+      <p class="intro_techName">HTML5</p>
+    </li>
+    <li class="intro_techListItem">
+      <i class="fab fa-css3 intro_techIcon"></i>
+      <p class="intro_techName">CSS3</p>
+    </li>
+    <li class="intro_techListItem">
+      <i class="fab fa-js intro_techIcon"></i>
+      <p class="intro_techName">JavaScript</p>
+    </li>
+    <li class="intro_techListItem">
+      <i class="fab fa-react intro_techIcon"></i>
+      <p class="intro_techName">React</p>
+    </li>
+    <li class="intro_techListItem">
+      <i class="fab fa-php intro_techIcon"></i>
+      <p class="intro_techName">PHP</p>
+    </li>
+    <li class="intro_techListItem">
+      <i class="fab fa-sass intro_techIcon"></i>
+      <p class="intro_techName">SASS</p>
+    </li>
+  </ul>
+</section>
 
 <?php include("includes/footer.inc"); ?>
 <?php include("includes/html_footer.inc"); ?>
