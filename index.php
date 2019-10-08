47cf169b0d7d102f36b48d9e424ddc9f53628a8f
Sharon Hartsell <sharon.d.hartsell@gmail.com>
2019-10-07 20:16:17 -0400

    chore(index): separate boilerplate into PHP includes

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include("includes/html_header.inc"); ?>
+<?php include("includes/header.inc"); ?>
+
+<p>Hello world! This is HTML5 Boilerplate.</p>
+
+<?php include("includes/footer.inc"); ?>
+<?php include("includes/html_footer.inc"); ?>
