91deaaa4d9974fbf8170063828c958d548629069
Sharon Hartsell <sharon.d.hartsell@gmail.com>
2019-10-20 18:44:48 -0400

    style(index): refine section layout

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include("includes/header.inc"); ?>
 
 <!-- Intro -->
-<section class="intro section--fullHt">
+<section class="intro">
   <img src="https://via.placeholder.com/300" class="intro_image" alt="Sharon" />
   <h1 class="intro_headline t-title--60b">Hi! I’m Sharon, a front-end web developer.</h1>
   <ul class="intro_techList">
@@ -50,13 +50,15 @@ $work = [
   ]
 ];
 ?>
-<section class="work section--fullHt">
-  <h2 class="t-title--40l">&lt; work &gt;</h2>
+<section class="work">
+  <h2 class="work_sectionTitle t-title--40l">&lt; work &gt;</h2>
   <ul class="work_projectList">
     <?php foreach($work as $machine_name => $work_info) : ?>
       <li class="work_projectListItem">
-        <h3 class="work_projectTitle t-title--30b"><?php print $work_info['title']; ?></h3>
-        <p class="t-body--18l"><?php print $work_info['teaser']; ?></p>
+        <div class="work_projectTextWrapper">
+          <h3 class="work_projectTitle t-title--30b"><?php print $work_info['title']; ?></h3>
+          <p class="work_projectTeaser t-body--18l"><?php print $work_info['teaser']; ?></p>
+        </div>
         <a href="/work/<?php print $machine_name; ?>.php" class="work_projectBtn btn--primary t-title--20b t-caps">See Details</a>
       </li>
     <?php endforeach; ?>
@@ -64,26 +66,28 @@ $work = [
 </section>
 
 <!-- About -->
-<section class="about section--fullHt">
-  <h2 class="t-title--40l">&lt; about &gt;</h2>
+<section class="about">
+  <h2 class="about_sectionTitle t-title--40l">&lt; about &gt;</h2>
   <div class="about_contentWrapper">
-    <img src="https://via.placeholder.com/500" class="about_image" alt="Sharon" />
-    <div class="about_text">
-      <p class="t-body--18l">Lorem ipsum dolor est sit amet.</p>
-      <button class="btn--secondary t-title--20b t-caps">Download resume</button>
-      <div>
-        <a href="#"><i class="fab fa-linkedin"></i></a>
-        <a href="#"><i class="fab fa-github"></i></a>
-        <a href="#"><i class="fab fa-codepen"></i></a>
+    <div class="about_imageWrapper">
+      <img src="https://via.placeholder.com/500" class="about_image" alt="Sharon" />
+    </div>
+    <div class="about_textWrapper">
+      <p class="about_text t-body--18l">Hi! I’m Sharon, a front-end web developer who specializes in creating user-friendly and engaging web experiences. I’ve been in the field for nearly two years, and I love learning new things, especially why things work the way they do. I’m a diligent problem solver who pays keen attention to details, partly due to my background in graphic design. Check out some of my latest work above, and contact me via the form below or directly at <a href="mailto:hello@sharonhartsell.com">hello@sharonhartsell.com</a>.</p>
+      <button class="about_resumeBtn btn--secondary t-title--20b t-caps">Download resume</button>
+      <div class="about_socialLinks">
+        <a href="#" class="about_socialLink"><i class="fab fa-linkedin"></i></a>
+        <a href="#" class="about_socialLink"><i class="fab fa-github"></i></a>
+        <a href="#" class="about_socialLink"><i class="fab fa-codepen"></i></a>
       </div>
     </div>
   </div>
 </section>
 
 <!-- Contact -->
-<section class="contact section--fullHt">
-  <h2 class="t-title--40l">&lt; contact &gt;</h2>
-  <p class="t-body--18l">Get in touch by emailing hello@sharonhartsell.com or by filling out the form below.</p>
+<section class="contact">
+  <h2 class="contact_sectionTitle t-title--40l">&lt; contact &gt;</h2>
+  <p class="contact_instructions t-body--18l">Get in touch by emailing hello@sharonhartsell.com or by filling out the form below.</p>
   <form method="post">
     <div>
       <label for="contactForm_name">Name</label>
@@ -97,7 +101,7 @@ $work = [
       <label for="contactForm_msg">Message</label>
       <textarea id="contactForm_msg" name="msg"></textarea>
     </div>
-    <button type="submit" class="t-title--20b t-caps">Send Message</button>
+    <button type="submit" class="btn--primary t-title--20b t-caps">Send Message</button>
   </form>
   
   <?php if (!empty($_POST)) : ?>
