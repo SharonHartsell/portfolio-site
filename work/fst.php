ed4ffb6cc09b7e5cc087f49d6f3c22d81fea2cb9
Sharon Hartsell <sharon.d.hartsell@gmail.com>
2019-10-29 21:05:36 -0400

    feat(fst): update written content

--- work/fst.php
+++ work/fst.php
@@ -3,28 +3,33 @@
 include('../includes/html_header.inc'); 
 include('../includes/header.inc');
 
-$sample_code = file_get_contents('./code_samples/fst-package-list.php');
+// $sample_code = file_get_contents('./code_samples/fst-package-list.php');
 
 $content = [
   'machine_name' => 'fst',
 
   // Intro
   'title' => 'Finishings Selection Tool',
-  'summary' => 'Life Plan Communities often offer their new residents choices to customize their new home. This tool allows these residents to visualize their chosen paint colors, cabinets, flooring, and finishings all in one place. The Finishings Selection Tool also captures pricing information.',
-  'date' => '2018-19',
+  'summary' => 'Life Plan Communities often offer their new residents choices to customize their new home. This tool allows these residents to visualize their chosen paint colors, cabinets, flooring, and other finishings all in one place. It also calculates customization costs and provides the builder a reference during construction.',
+  'date' => '2017, 2019',
   'company' => 'GlynnDevins',
   'live_link' => '',
   'github_link' => '',
-  'tech' => ['html5', 'css3', 'sass', 'js', 'jquery', 'php', 'drupal'],
+  'tech' => ['html5', 'css3', 'js', 'sass', 'php', 'drupal'],
   'intro_img' => 'fst-mac.png',
 
   'my_role' => [
-    'Style custom Drupal forms',
-    'Customized PHP theme templates',
-    'Built and styled HTML email',
-    'Implemented skeleton screens',
-    'CSS Grid and Flexbox', 
-    'HTML, CSS, JS',
+    'HTML/CSS' => [
+      'CSS Grid and Flexbox',
+      'Custom PHP Drupal theme templates',
+      'Custom Drupal forms',
+      'Dynamic email templates',
+    ],
+    'JavaScript with jQuery' => [],
+    'Design' => [
+      'UI/UX design',
+      'Skeleton screens',
+    ],
   ],
   
   // Slideshow
@@ -42,7 +47,7 @@ $content = [
   ],
 
   // Code
-  'code_sample' => $sample_code,
+  // 'code_sample' => $sample_code,
 ];
 
 include('../includes/work_detail.inc');
